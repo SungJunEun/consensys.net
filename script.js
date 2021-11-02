1049863d28533bc9de1b669b942e60e2c3e830cb
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-11-02 14:51:24 +0000

    Navbar scroll background event

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const announcebar = document.querySelector('.announcebar');
 const announcebarBtn = document.querySelector('.announcebar__btn');
-
+const navbar = document.querySelector('.navbar');
 // remove announcebar if its clicked
 announcebarBtn.addEventListener('click', function(e) {
   e.preventDefault();
   announcebar.remove();
 })
+
+window.addEventListener('scroll', function() {
+  if(!window.scrollY) {
+    navbar.style.backgroundColor = 'transparent';
+  }
+  else {
+    navbar.style.backgroundColor = '#202328';
+  }
+});
