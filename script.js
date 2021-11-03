d2cc0da5763cdd600b1c3cca53abfa41599de746
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-11-03 13:10:15 +0000

    HTML,CSS: Search page

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const body = document.querySelector('body');
 const announcebar = document.querySelector('.announcebar');
 const announcebarBtn = document.querySelector('.announcebar__btn');
 const navbar = document.querySelector('.navbar');
+const navbarBtns = document.querySelector('.navbar__btns');
+const searchPage = document.querySelector('.search__page');
 // remove announcebar if its clicked
 announcebarBtn.addEventListener('click', function(e) {
   e.preventDefault();
   announcebar.remove();
+  navbar.style.top = '0px';
 })
-
+//scroll event handle
 window.addEventListener('scroll', function() {
   if(!window.scrollY) {
     navbar.style.backgroundColor = 'transparent';
@@ -14,4 +18,24 @@ window.addEventListener('scroll', function() {
   else {
     navbar.style.backgroundColor = '#202328';
   }
+});
+
+//navbar button click event
+navbarBtns.addEventListener('click', function(e) {
+  let id = e.target.id;
+  if(e.target.tagName == 'I') {
+    id = e.target.parentNode.id;
+  }
+  console.log(id);
+  if(!id) {
+    return;
+  }
+  else if(id == "search") {
+    searchPage.style.display = "inline-block";
+    body.style.overflow = "hidden";
+  }
+  else if(id == "login") {
+
+  }
+
 });
